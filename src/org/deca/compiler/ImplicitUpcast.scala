588f9a257af6e59596464004a9d8ca891739b810
Eli Gottlieb <eligottlieb@gmail.com>
2011-11-02 22:17:50 -0400

    Added back compilation of implicit upcasts.

---
+++ src/org/deca/compiler/ImplicitUpcast.scala
@@ -0,0 +1,68 @@
+package org.deca.compiler
+
+import org.jllvm._
+import org.jllvm.bindings._
+
+class ImplicitUpcast(val expression: Expression,upcast: MonoType) extends Expression {
+  expType = upcast
+  assert(TypeOrdering.lteq(expression.expType,expType))
+  override def constrain(scs: SignatureConstraints): Unit = Unit
+  override def check(scs: SignatureConstraints): Unit = Unit
+  
+  override def substitute(sub: SignatureSubstitution): Unit =
+    expType = sub.solve(expType).asInstanceOf[MonoType]
+  override def specialize(spec: SignatureSubstitution): Expression =
+    new ImplicitUpcast(expression.specialize(spec),spec.solve(upcast).asInstanceOf[MonoType])
+  override val children: List[Expression] = (expression :: Nil)
+  override def compile(builder: LLVMInstructionBuilder,scope: Scope,instantiation: Module): LLVMValue = if(TypeOrdering.lt(expression.expType,expType)) {
+    val child = expression.compile(builder,scope,instantiation)
+    (expression.expType,expType) match {
+      case (unsignedx: UnsignedIntegerType,realy: RealType) => {
+        val doubled = new LLVMIntegerToFloatCast(builder,child,DoubleType.compile,"cast",LLVMIntegerToFloatCast.IntCastType.UNSIGNED)
+        new LLVMExtendCast(LLVMExtendCast.ExtendType.FLOAT,builder,doubled,realy.compile,"cast")
+      }
+      
+      case (integerx: IntegerType,realy: RealType) => {
+        val doubled = new LLVMIntegerToFloatCast(builder,child,DoubleType.compile,"cast",LLVMIntegerToFloatCast.IntCastType.SIGNED)
+        new LLVMExtendCast(LLVMExtendCast.ExtendType.FLOAT,builder,doubled,realy.compile,"cast")
+      }
+      
+      case (realx: RealType,realy: RealType) => new LLVMExtendCast(LLVMExtendCast.ExtendType.FLOAT,builder,child,realy.compile,"cast")
+      
+      //TODO: Fill in the rest of these cases.
+      case (sumx: SumType,sumy: SumType) => (sumx.enumeration,sumy.enumeration) match {
+        case (true,true) => new LLVMExtendCast(LLVMExtendCast.ExtendType.ZERO,builder,child,sumy.compile,"cast")
+        case (true,false) => new LLVMInsertValueInstruction(builder,new LLVMUndefinedValue(sumy.compile),new LLVMExtendCast(LLVMExtendCast.ExtendType.ZERO,builder,child,sumy.tagRepresentation,"cast"),0,"variant")
+        case (false,false) => {
+          //Insert the tag into the resulting variant.
+          val tag = new LLVMExtractValueInstruction(builder,child,0,"extract")
+          val variant = new LLVMInsertValueInstruction(builder,new LLVMUndefinedValue(sumy.compile),tag,0,"insert")
+          
+          //Allocate memory space for the resulting variant into which to stuff the old data.
+          val result = new LLVMStackAllocation(builder,sumy.compile,LLVMConstantInteger.constantInteger(Nat.compile,1,false),"nonsense")
+          new LLVMStoreInstruction(builder,variant,result)
+          
+          //Get the pointer to the new variant's contents, fetch the old variant's contents, cast the pointer, and store the old into the new.
+          val indices = List(LLVMConstantInteger.constantInteger(Nat.compile,0,false),LLVMConstantInteger.constantInteger(Nat.compile,1,false))
+          var pContents: LLVMValue = new LLVMGetElementPointerInstruction(builder,result,indices.toArray,"gep")
+          val oldContents = new LLVMExtractValueInstruction(builder,child,1,"extract")
+          pContents = new LLVMBitCast(builder,pContents,new LLVMPointerType(oldContents.typeOf,0),"pointer_cast")
+          new LLVMStoreInstruction(builder,oldContents,pContents)
+          //Return the entire new variant.
+          new LLVMLoadInstruction(builder,result,"load")
+        }
+      }
+      
+      case (px: PointerType,py: PointerType) => new LLVMBitCast(builder,child,py.compile,"pointer_cast")
+      
+      case (ux: UnsignedIntegerType,uy: UnsignedIntegerType) => new LLVMExtendCast(LLVMExtendCast.ExtendType.ZERO,builder,child,uy.compile,"cast")
+      case (ix: IntegerType,iy: IntegerType) => new LLVMExtendCast(LLVMExtendCast.ExtendType.SIGN,builder,child,iy.compile,"cast")
+      
+      case (_,UnitType) => child
+      
+      case _ => throw new Exception("Unknown implicit upcast from " + expression.expType.toString + " to " + expType.toString)
+    }
+  }
+  else
+    expression.compile(builder,scope,instantiation)
+}
