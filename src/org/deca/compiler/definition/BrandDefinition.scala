21f18cc11169480ed779f9968a7ed5fc208c8ea5
Eli Sennesh <eligottlieb@gmail.com>
2013-07-25 15:39:00 +0300

    Tree creation for variant definitions.

--- src/org/deca/compiler/definition/BrandDefinition.scala
+++ src/org/deca/compiler/definition/BrandDefinition.scala
@@ -8,7 +8,7 @@ import org.deca.compiler.definition._
 import org.deca.compiler.expression._
 
 object EnumerationClasses {
-  val apply: Memoize4[Module, String, Int, ClassBrand, ClassBrand] = Memoize4((scope: Module, name: String, tag: Int, parent: ClassBrand) => {
+  val apply: Memoize4[Module, String, Int, Option[ClassBrand], ClassBrand] = Memoize4((scope: Module, name: String, tag: Int, parent: Option[ClassBrand]) => {
     val res = ClassBrands.apply(scope, name, parent, Nil, Nil)
     res.seal(Map(name -> tag))
     res
@@ -16,16 +16,16 @@ object EnumerationClasses {
 }
 
 object ClassBrands {
-  val apply = Memoize5((scope: Scope, name: String, parent: ClassBrand, members: List[MemberDeclaration], methods: List[MethodDeclaration]) => {
+  val apply = Memoize5((scope: Scope, name: String, parent: Option[ClassBrand], members: List[MemberDeclaration], methods: List[MethodDeclaration]) => {
     val recordMembers: List[RecordMember] = members.map(member => RecordMember(Some(member.name), member.mutability, member.tau))
-    new ClassBrand(name, new RecordType(recordMembers), methods.map(method => (method.name, method.signature.arrow.asInstanceOf[FunctionPointer])).toMap, Some(parent))
+    new ClassBrand(name, new RecordType(recordMembers), methods.map(method => (method.name, method.signature.arrow.asInstanceOf[FunctionPointer])).toMap, parent)
   })
 }
 
-class EnumValueDefinition(scope: Module, name: String, val tag: Int, val parent: ClassBrand) extends VariableDefinition(scope, name, new EnumerationValue(scope, name, tag, parent), new BrandType(EnumerationClasses.apply(scope, name, tag, parent), EmptyRecord), ImmutableMutability)
+class EnumValueDefinition(scope: Module, name: String, val tag: Int, val parent: ClassBrand) extends VariableDefinition(scope, name, new EnumerationValue(scope, name, tag, parent), new BrandType(EnumerationClasses.apply(scope, name, tag, Some(parent)), EmptyRecord), ImmutableMutability)
 
 class EnumerationValue(scope: Module, val enum: String, val tag: Int, val parent: ClassBrand) extends ConstantExpression {
-  val brand: ClassBrand = EnumerationClasses.apply(scope, enum, tag, parent)
+  val brand: ClassBrand = EnumerationClasses.apply(scope, enum, tag, Some(parent))
   expType = new BrandType(brand, EmptyRecord)
   override val children = Nil
   override def build(scope: Scope,instantiation: Module): LLVMConstantInteger =
@@ -50,10 +50,10 @@ class ClassDefinition(override val scope: Module,
                       override val name: String,
                       arguments: List[(String, MonoType)],
                       implicits: List[(String, MonoType)],
-                      val parent: ClassDefinition,
+                      val parent: Option[ClassDefinition],
                       val fields: List[MemberDeclaration],
                       methodDeclarations: List[MethodDeclaration]) extends Definition {
-  val brand: ClassBrand = ClassBrands.apply(scope, name, parent.brand, fields, methodDeclarations)
+  val brand: ClassBrand = ClassBrands.apply(scope, name, parent.map(_.brand), fields, methodDeclarations)
   for(method <- methodDeclarations)
     method.addThis(brand)
   def thisPointer: PointerType = new PointerType(new BrandType(brand, EmptyRecord), new RegionVariable(false), ReadOnlyMutability)
@@ -70,4 +70,4 @@ class ClassDefinition(override val scope: Module,
   override val build: Memoize1[Module, Set[LLVMValue]] = constructor.build
 }
 
-class DataConstructorDefinition(scope: Module, name: String, arguments: List[(String, MonoMutability, MonoType)], parent: ClassDefinition) extends ClassDefinition(scope, name, arguments.map(arg => (arg._1, arg._3)), Nil, parent, MemberDeclaration.dataConstructors(arguments), Nil)
+class DataConstructorDefinition(scope: Module, name: String, arguments: List[(String, MonoMutability, MonoType)], parent: ClassDefinition) extends ClassDefinition(scope, name, arguments.map(arg => (arg._1, arg._3)), Nil, Some(parent), MemberDeclaration.dataConstructors(arguments), Nil)
